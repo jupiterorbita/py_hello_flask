7970083af487d769a61d9369c647fa0d1f696895
John M <John.williams@outlook.com>
2020-11-30 17:52:20 -0700

    hello flask

--- hello.py
+++ hello.py
@@ -4,6 +4,24 @@ app = Flask(__name__) #We made an instance of the Flask class called "app" Creat
 @app.route('/') # The "@" decorator associates this route with the function immediately following
 def hello_world():
   return 'Hello World hi' # Return the string 'Hello World!' as a response
+# if the client requests localhost:5000/, the hello_world function will run
+
+@app.route('/success')
+def success():
+  return "success!"
+
+# VARIABLE NAMES
+@app.route("/hello/<name>")# for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+def hello(name):
+  print(name)
+  return f"Hello, {name}"
+
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+  print('username ->', username)
+  print('id       ->', id)
+  return "username: " + username + ", id:" + id
+
 
 if __name__=="__main__": # Ensure this file is being run directly and not from a different module    
   app.run(debug=True) # Run the app in debug mode.
