af8800af89a24bbd25afa2271166b943496efc3a
John M <John.williams@outlook.com>
2020-11-30 17:29:12 -0700

    hello flask

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask #We imported the Flask class. Import Flask to allow us to create our app
+app = Flask(__name__) #We made an instance of the Flask class called "app" Create a new instance of the Flask class called "app"
+
+@app.route('/') # The "@" decorator associates this route with the function immediately following
+def hello_world():
+  return 'Hello World hi' # Return the string 'Hello World!' as a response
+
+if __name__=="__main__": # Ensure this file is being run directly and not from a different module    
+  app.run(debug=True) # Run the app in debug mode.
